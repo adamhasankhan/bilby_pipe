16117865df1302c44bb40aafe4e6b14e16cc2d15
Gregory Ashton <gregory.ashton@ligo.org>
2023-01-24 08:44:18 +0000

    Quality of life improvements

--- bilby_pipe/job_creation/bilby_pipe_dag_creator.py
+++ bilby_pipe/job_creation/bilby_pipe_dag_creator.py
@@ -30,7 +30,7 @@ def get_trigger_time_list(inputs):
         trigger_times = start_times + inputs.duration - inputs.post_trigger_duration
     else:
         raise BilbyPipeError("Unable to determine input trigger times from ini file")
-    logger.info(f"Setting segment trigger-times {trigger_times}")
+    logger.debug(f"Setting segment trigger-times {trigger_times}")
     return trigger_times
 
 
