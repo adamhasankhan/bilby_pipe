52157476d3e365837d99dfc89d6a97e1dc252028
michael.williams <michael.williams@ligo.org>
2025-06-24 16:21:59 +0100

    MAINT: move environment logic

--- bilby_pipe/job_creation/node.py
+++ bilby_pipe/job_creation/node.py
@@ -8,12 +8,10 @@ import pycondor
 
 from ..utils import (
     CHECKPOINT_EXIT_CODE,
-    DEFAULT_GWDATAFIND_SERVER,
-    ENVIRONMENT_DEFAULTS,
     ArgumentsString,
     BilbyPipeError,
     logger,
-    sanitize_string_for_list,
+    get_environment_variables_dictionary,
 )
 
 
@@ -174,6 +172,8 @@ class Node(object):
 
         # Hack to allow passing walltime down to slurm
         setattr(self.job, "slurm_walltime", self.slurm_walltime)
+        # Horrible hack 
+        setattr(self.job, "slurm_environment", self.environment)
 
         logger.debug(f"Adding job: {job_name}")
 
@@ -257,51 +257,17 @@ class Node(object):
 
     @property
     def environment(self):
-        f"""
-        Environment variables to set in jobs.
-        This starts from {ENVIRONMENT_DEFAULTS} and adds values from the
-        :code:`--environment-variables` and :code:`--getenv` arguments.
-        """
-        env = ENVIRONMENT_DEFAULTS.copy()
-        for key in self.inputs.getenv:
-            value = os.environ.get(key, None)
-            if value is not None:
-                env[key] = sanitize_string_for_list(str(value))[0]
-            else:
-                logger.warning(
-                    f"Variable {key} requested from getenv, "
-                    "but not found in environment."
-                )
-        env.update(self.inputs.environment_variables)
-        if self.inputs.disable_hdf5_locking:
-            logger.warning(
-                "The --disable-hdf5-locking variable is deprecated use, "
-                "--environment-variables instead."
-            )
-            env["HDF5_USE_FILE_LOCKING"] = "FALSE"
+        f"""Environment variables to set in jobs.
 
-        # Set the GWDATAFIND_SERVER environment variable for use in jobs.
-        # If data_find_url is not set, use the environment variable
-        # GWDATAFIND_SERVER if it is included in `environment-variables`
-        # otherwise use the default from bilby_pipe.utils
-        if self.inputs.data_find_url is None:
-            if "GWDATAFIND_SERVER" not in env:
-                logger.debug(
-                    (
-                        "`data-find-url` is not specified and `environment-variables` "
-                        "does not include GWDATAFIND_SERVER. "
-                        f"Using the default value: {DEFAULT_GWDATAFIND_SERVER}"
-                    )
-                )
-                env["GWDATAFIND_SERVER"] = DEFAULT_GWDATAFIND_SERVER
-        else:
-            if "GWDATAFIND_SERVER" in env:
-                logger.warning(
-                    "GWDATAFIND_SERVER is specified in `environment-variables` "
-                    "and via the `data-find-url` argument. The latter will be used."
-                )
-            env["GWDATAFIND_SERVER"] = self.inputs.data_find_url
-        return env
+        See :code:`bilby_pipe.utils.get_environment_variables_dictionary`
+        for more details on how the environment variables are determined.
+
+        .. version-changed:: 1.7.0
+              The environment variables are now determined by the
+              :code:`bilby_pipe.utils.get_environment_variables_dictionary`
+              function.
+        """
+        return get_environment_variables_dictionary(self.inputs)
 
     @property
     def transfer_container(self):
